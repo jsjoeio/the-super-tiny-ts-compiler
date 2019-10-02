c6c73f96cd2a897f3ce1f1a86966f55e28fa49de
Joe Previte <jjprevite@gmail.com>
2019-10-02 16:01:31 -0700

    feat: write transformer

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -3,29 +3,45 @@ import { Token } from '../tokenizer';
 export type ASTNode = {
   // this is a type literal
   type: 'Program';
-  body: Array<NodeOrCallExpressionNode>;
-}
+  body: Node[];
+  _context?: Node[];
+};
 
 export type CallExpressionNode = {
   type: 'CallExpression';
   name: string;
   params: Node[];
+  _context?: Array<CallExpressionNode | TransformedCallExpressionNode | Node>;
+};
+
+export type TransformedCallExpressionNode = {
+  type: 'CallExpression';
+  callee: {
+    type: 'Identifier';
+    name: string;
+  };
+  arguments: any[];
+};
+
+export type ExpressionStatementNode = {
+  type: 'ExpressionStatement';
+  expression: TransformedCallExpressionNode;
 };
 
 export type StringLiteralNode = {
   type: 'StringLiteral';
   value: string;
-}
+  _context?: Node[];
+};
 
 export type NumberLiteralNode = {
   type: 'NumberLiteral';
   value: string;
-}
+  _context?: Node[];
+};
 
 // discriminated union type
-export type Node = ASTNode | CallExpressionNode | StringLiteralNode | NumberLiteralNode
-// This is a union type
-type NodeOrCallExpressionNode = Node | CallExpressionNode;
+export type Node = ASTNode | CallExpressionNode | StringLiteralNode | NumberLiteralNode;
 /**
  * ============================================================================
  *                                 ヽ/❀o ل͜ o\ﾉ
---
+++ src/transformer/index.ts
@@ -0,0 +1,107 @@
+import {
+  ASTNode,
+  Node,
+  NumberLiteralNode,
+  StringLiteralNode,
+  CallExpressionNode,
+  TransformedCallExpressionNode,
+} from '../parser';
+import { traverser } from '../traverser';
+import { ExpressionStatementNode } from '../parser/index';
+
+export function transformer(ast: ASTNode) {
+  // We'll create a newAST
+  let newAst: ASTNode = {
+    type: 'Program',
+    body: [],
+  };
+
+  // the context is a reference from the old ast to the new ast
+  ast._context = newAst.body;
+
+  traverser(ast, {
+    // The first visitor method accepts any 'NumebrLiteral'
+    NumberLiteral: {
+      // We'll visit them on enter
+      enter(node: NumberLiteralNode, parent: Node) {
+        // We'll create a new node also named 'NumberLiteral' that we will push
+        // to the parent context
+        if (parent._context) {
+          parent._context = parent._context.concat({
+            type: 'NumberLiteral',
+            value: node.value,
+          });
+        } else {
+          parent._context = [
+            {
+              type: 'NumberLiteral',
+              value: node.value,
+            },
+          ];
+        }
+      },
+    },
+
+    // Next we'll have a StringLiteral
+    StringLiteral: {
+      enter(node: StringLiteralNode, parent: Node) {
+        if (parent._context) {
+          parent._context = parent._context.concat({
+            type: 'StringLiteral',
+            value: node.value,
+          });
+        } else {
+          parent._context = [
+            {
+              type: 'StringLiteral',
+              value: node.value,
+            },
+          ];
+        }
+      },
+    },
+
+    // Next up, CallExpression
+    CallExpression: {
+      enter(node: CallExpressionNode, parent: Node) {
+        // we'll start by creating a new node CallExpression with a nested Identifier
+        let expression: TransformedCallExpressionNode | ExpressionStatementNode = {
+          type: 'CallExpression',
+          callee: {
+            type: 'Identifier',
+            name: node.name,
+          },
+          arguments: [],
+        };
+
+        // Next we're going to define a new context on the original
+        // to erefence the expression's arguments
+        // so that we can push arguments.
+        node._context = expression.arguments;
+
+        // Then we're going to check if the parent node is a CallExpression
+        // If iti s not
+        if (parent.type !== 'CallExpression') {
+          // We're going to wrap our CallExpression with an ExpressionStatement
+          // We do this because the top level CallExprssion in JavaScript are actually Statements
+          expression = {
+            type: 'ExpressionStatement',
+            expression: expression,
+          };
+        }
+
+        // Last we'll push our (possibly wrapped) CallExpression to the parents context
+        if (parent._context) {
+          // @ts-ignore
+          parent._context = parent._context.concat(expression);
+        } else {
+          // @ts-ignore
+          parent._context = [expression];
+        }
+      },
+    },
+  });
+
+  // At the end of our transformer, we'll return the new ast that we just created
+  return newAst;
+}
